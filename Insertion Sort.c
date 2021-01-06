265b976c4d534184ae2799d86baedfd636b62630
Vishal-Kumar <67988127+Vishal-ku@users.noreply.github.com>
2021-01-07 01:29:26 +0530

    Update Insertion Sort.c

--- Insertion Sort.c
+++ Insertion Sort.c
@@ -8,17 +8,15 @@ void main()
    printf("\nEnter the elements..\n");
    for(i=0; i<n; i++)
     scanf("%d", &a[i]);
-   for(i=0; i<n-1; i++)
+   for(i=1; i<n; i++)
    {
-       for(j=i+1; j<n; j++)
-       {
-           if(a[i]>a[j])
-           {
-               temp=a[i];
-               a[i]=a[j];
-               a[j]=temp;
-           }
-       }
+      int current=a[i];
+      int j=i-1;
+      while(a[j]>current && j>=0){
+        a[j+1]=a[j];
+        j--;
+      }
+      a[j+1]=current;
 
    }
    for(i=0; i<n; i++)
